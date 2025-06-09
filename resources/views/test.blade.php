f6292a8bda8752c5271164a24ea5e258e0275078
Marek Walica <mwalica@gmail.com>
2025-06-09 20:25:51 +0200

    add test

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello</title>
+</head>
+<body>
+<h3>Test</h3>
+{{date()}}
+</body>
+</html>
